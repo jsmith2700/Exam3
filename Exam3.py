b9a31bbe1827992f5d1c2e2dd8732b04cae14276
jsmith2700 <98788736+jsmith2700@users.noreply.github.com>
2022-03-09 12:14:06 -0600

    Update and rename README.md to Exam3.py

---
+++ Exam3.py
@@ -0,0 +1,26 @@
+# Exam3
+def prime(x, y):
+    prime_list = []
+    for i in range(x, y):
+        if i == 0 or i == 1:
+            continue
+        else:
+            for j in range(2, int(i/2)+1):
+                if i % j == 0:
+                    break
+            else:
+                prime_list.append(i)
+    return prime_list
+ 
+print("The starting range is:", end=' ')
+print(starting_range)
+print("The ending range is:", end=' ')
+print(ending_range)
+
+
+
+lst = prime(starting_range, ending_range)
+if len(lst) == 0:
+    print("There are no prime numbers in this range")
+else:
+    print("The prime numbers in this range are: ", lst)
